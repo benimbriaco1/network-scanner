3386661e547d7fa89481876236b90135ad482c41
benimbriaco1 <benimbriaco06@gmail.com>
2025-02-21 10:49:10 -0500

    formatted

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 # packet manipulation
 from scapy.all import ARP, Ether, srp
 
-# our target IP address, mask of /24
-target_ip = "192.168.1.1/24"
+# our target IP address
+# takes in user input
+target_ip = input("Enter IP address and subnet in the following form: xxx.xxx.xxx.xxx/xx\n")
 
 # create an ARP packet
+# pdst = protocol destination address
 arp = ARP(pdst=target_ip)
 
 # creating the ethernet broadcast packet
 ether = Ether(dst="ff:ff:ff:ff:ff:ff")
 
 # stacking the two packets using /
+# here we are encapsulating arp packet within ethernet frame
 packet = ether/arp
 
 # srp() is used to send and receive packets at layer 2 
@@ -30,4 +33,11 @@ for sent, received in result:
     # using a dictionary with key value pairs to add
     clients.append({'ip': received.psrc, 'mac': received.hwsrc})
 
-print(clients)
+# formatting
+print("Available devices on the network:")
+# " "*24 adds 24 spaces
+print("IP" + " "*18+"MAC")
+# iterate through to print
+for client in clients:
+    # ensures client['ip'] is left aligned and takes up 16 characters
+    print("{:16}    {}".format(client['ip'], client['mac']))
